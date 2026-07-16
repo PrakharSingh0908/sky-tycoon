255b6f9630c466e0764499e5c7a42bf9ef1868fb
Prakhar Singh <prakharsingh0908@gmail.com>
2026-07-16 12:55:44 +0530

    Initial Commit

---
+++ SkyTycoon/SkyTycoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyTycoonApp.swift
+//  SkyTycoon
+//
+//  Created by Prakhar Singh on 16/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyTycoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
